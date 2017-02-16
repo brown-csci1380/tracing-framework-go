88da6739f6bd40b3fa1cf3d3757d3f9cc90460f8
Carlos Rotger <carlos_rotger@brown.edu>
2017-02-15 22:17:19 -0500

    cleaned up concurrency behavior to be more correct

--- xtrace/client/channel.go
+++ xtrace/client/channel.go
@@ -5,7 +5,7 @@ import (
 )
 
 var registeredChannels map[interface{}]chan int64 = make(map[interface{}]chan int64)
-var rcLock *sync.RWMutex = &sync.RWMutex{}
+var rcLock sync.Mutex
 
 const BUF = 2
 
@@ -14,14 +14,12 @@ const BUF = 2
 // from. returns a chan int64 which will emit every eventid that has registered as sending
 // along the channel
 func RegisterChannelReciever(channel interface{}) (ch chan int64) {
-	rcLock.RLock()
+	rcLock.Lock()
+	defer rcLock.Unlock()
 	ch, ok := registeredChannels[channel]
-	rcLock.RUnlock()
 	if !ok {
 		ch = make(chan int64, BUF)
-		rcLock.Lock()
 		registeredChannels[channel] = ch
-		rcLock.Unlock()
 	}
 	return
 }
@@ -35,13 +33,13 @@ func AddChannelEvent(channel interface{}) {
 
 // Get the last EventID that sent a value along the provided channel.
 // Returns a singleton slice containing the most recent EventID of the sender,
-// so long as the sender called called RegisterChannelSender before sending
+// so long as the sender called called SendChannelEvent before sending
 // the value. Returns an empty slice if no sender is known.
 func GetChannelSender(channel interface{}) []int64 {
 	ch := RegisterChannelReciever(channel)
 
 	select {
-	// if the recv blocks, there must have been no call to RegisterChannelSender on the
+	// if the recv blocks, there must have been no call to SendChannelEvent on the
 	// channel as the function argument
 	case sender := <-ch:
 		return []int64{sender}
@@ -50,30 +48,20 @@ func GetChannelSender(channel interface{}) []int64 {
 	}
 }
 
-// called by the sender of a value over a channel. Informs the future recipient of the value
-// which event ID it originated from
-func RegisterChannelSender(channel interface{}) {
-	rcLock.RLock()
+// called by the sender of a value over a channel BEFORE sending the value.
+// Informs the future recipient of the value which event ID it originated from
+func SendChannelEvent(channel interface{}) {
+	rcLock.Lock()
+	defer rcLock.Unlock()
 	ch, ok := registeredChannels[channel]
-	rcLock.RUnlock()
 
 	if !ok {
 		ch = make(chan int64, BUF)
-		rcLock.Lock()
 		registeredChannels[channel] = ch
-		rcLock.Unlock()
 	}
 	eventID := GetEventID()
-
-	select {
-	case ch <- eventID:
-		// sent immediately
-		return
-	default:
-		// do this in a separate goroutine because chan sends can block unless there is a reciever ready
-		go func() {
-			ch <- eventID
-		}()
-	}
-
+	// do this in a separate goroutine because chan sends can block unless there is a reciever ready
+	go func() {
+		ch <- eventID
+	}()
 }
